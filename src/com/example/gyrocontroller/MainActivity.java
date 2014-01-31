06723a2c817adc735730b17dfd242dffdb98fe9a
zeuz113 <zeuz113@gmail.com>
2014-01-31 17:15:10 -0500

    init test

---
+++ src/com/example/gyrocontroller/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.gyrocontroller;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.view.Menu;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("http://10.1.10.50:1234/gyro.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
